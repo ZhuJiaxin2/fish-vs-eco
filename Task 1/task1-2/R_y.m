84e6f5d64615e5cbcdc629d4d34552bf0727e830
jiwy2020 <77949133+jiwy2020@users.noreply.github.com>
2024-02-03 16:56:38 +0800

    幼体密度计算成年体密度更新

--- Task 1/task1-2/R_y.m
+++ Task 1/task1-2/R_y.m
@@ -1,10 +1,10 @@
 function num = R_y(y, D_larval, D_juvenile, S_larval, beta_0, beta_1, l_bar, l_inf, d)
     num = 0;
-    for i = max(y - D_juvenile, 0):max(y, D_larval)
+    for i = max(y - D_juvenile, 0):min(y, D_larval)
         if i - 1 <= 0
             num = num + 1;
         else
-            num = num + P_y(i - 1, S_larval, beta_0, beta_1, l_bar, l_inf, d);
+            num = num + P_y(i - 1, S_larval, beta_0, beta_1, l_bar, l_inf, d) * S_larval * m_i(i, beta_0, beta_1, l_bar, l_inf, d);
         end
     end
 end
