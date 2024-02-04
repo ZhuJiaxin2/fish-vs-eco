19f41fe64b6b2d0bc2f94119e42bf00b824ddd26
jiwy2020 <77949133+jiwy2020@users.noreply.github.com>
2024-02-04 17:08:13 +0800

    Add files via upload

--- Task 2/ode.m
+++ Task 2/ode.m
@@ -4,36 +4,30 @@ ap = 1;
 au = 0.75;
 mp = 0.4;
 mu = 0.8;
-% mp = 0.5;
-% mu = 0.5;
 b = 0.3;
 r = 3;
 
 ode_system = @(t, y) [
-    ap * mp * min(y(1), y(2)) + b * (y(3) - y(1));
+    ap * mp * min(y(1), y(2)) + b * (y(3) - y(1)); %Npm
     ap * (1 - mp) * min(y(1), y(2)) + b * (y(4) / r - y(2));
-    ap * (1 - mp) * min(y(1), y(2)) + b * (y(4) / r - y(2));
-    au * mu * min(y(3), y(4)) + b * (y(1) - y(3));
+    au * mu * min(y(3), y(4)) + b * (y(1) - y(3)); %Num
     au * (1 - mu) * min(y(3), y(4)) + b * (y(2) * r - y(4));
-    ap * (1 - mp) * min(y(1), y(2)) + b * (y(6) - y(5));
-    au * (1 - mu) * min(y(3), y(4)) + b * (y(5) - y(6));
+    ap * (1 - mp) * min(y(1), y(2)) + b * (y(6) - y(5)); %Npf
+    au * (1 - mu) * min(y(3), y(4)) + b * (y(5) - y(6)); %Nuf
 ];
 
 
 tspan = [0 6]; % 从0到6年
 
-y0 = [1.5; 1.5; 1; 1.5; 1; 1]; % 初值
+y0 = [1.5; 1.5; 1; 1; 1.5; 1]; % 初值
 
 % ode45
 [t, y] = ode45(ode_system, tspan, y0);
 
-y = [y(:, 1), y(:, 5), y(:, 2), y(:, 6)];
-
+y_pic = [y(:,1), y(:,3), y(:,5), y(:,6)];
 figure;
-plot(t, y);
+plot(t, y_pic);
 xlabel('Time');
 ylabel('Num');
-legend('Npm', 'Npf', 'Num', 'Nuf');
+legend('Npm', 'Num', 'Npf', 'Nuf');
 title('env and sex rate');
-
-disp(sum(y(end, :)))
