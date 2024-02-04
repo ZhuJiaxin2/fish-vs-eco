d954d86a85722190548e089181b6f1a4f56bfa15
jiwy2020 <77949133+jiwy2020@users.noreply.github.com>
2024-02-04 22:00:59 +0800

    Update main.m

--- Task 1/task1-2/main.m
+++ Task 1/task1-2/main.m
@@ -1,14 +1,15 @@
 clear;close;clc
 i = 0;
-for Malerate = 40:1:80
+for Malerate = 1:1:90
     i = i + 1;
-    N_larval(i) = 0.0063 * Malerate - 0.1582;
+%     N_larval(i) = 0.0063 * Malerate - 0.1582; % 线性
+    N_larval(i) = -0.0000965 * Malerate ^ 2 + 0.0154956 * Malerate - 0.3658665;
     N_juvenile(i) = juvenile_density(N_larval(i));
 end
 
 matrix = deathnum(N_juvenile);
 
-x_values = 0.4:0.01:0.8;
+x_values = 0.01:0.01:0.9;
 
 % 绘制每组数据的曲线
 figure;
@@ -28,7 +29,8 @@ hold off;
 % 绘制平均曲线
 figure;
 average_curve = mean(matrix, 1);
-plot(x_values ./ (1 - x_values), average_curve, 'LineWidth', 2, 'DisplayName', 'Average Curve');
+xxx = x_values ./ (1 - x_values);
+plot(xxx, average_curve, 'LineWidth', 2, 'DisplayName', 'Average Curve');
 
 % 添加标题和标签
 title('Average Curve from a 37x11 Matrix');
