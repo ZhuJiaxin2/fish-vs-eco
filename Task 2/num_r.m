f1e291e9af7d61ca5ea27c4dea9bc8120d5220f4
jiwy2020 <77949133+jiwy2020@users.noreply.github.com>
2024-02-05 22:25:45 +0800

    en

--- Task 2/num_r.m
+++ Task 2/num_r.m
@@ -23,9 +23,9 @@ ode_system = @(t, y) [
 ];
 
 
-tspan = [0 6]; % 从0到6年
+tspan = [0 6]; % 0-6years
 
-y0 = [1.5; 1.5; 1; 1; 1.5; 1]; % 初值
+y0 = [1.5; 1.5; 1; 1; 1.5; 1]; % ordinary
 
 % ode45
 [t, y] = ode45(ode_system, tspan, y0);
